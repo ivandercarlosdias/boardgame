8d08dc8d1c415d29980aeca48efe0b2c7c899848
Ivander Carlos Dias <ivander@buildbox.com.br>
2024-08-30 09:51:02 -0300

    Init render players positions

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ class BoardGame {
    constructor(boardSize = 6) {
       this.boardSize = boardSize
    }
+
+   getBoardCellClasses(index) {
+      // adicionar classes na célula conforme posições e objetivos dos players
+      return `class-${index}`
+   }
 }
 
 document.addEventListener('alpine:init', () => {
